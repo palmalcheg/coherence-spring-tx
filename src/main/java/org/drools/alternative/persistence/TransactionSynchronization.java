7bff13a070db39fe333ed9f03233da0c0be18102
palmalcheg <palmal.mozhay@gmail.com>
2011-10-05 01:33:12 +0300

    Removed un-tested Drools functionality (need to be migrated to the latest 5.2.0 Drools API) Tx-Coherence testing, coherence-common utilizing.

--- src/main/java/org/drools/alternative/persistence/TransactionSynchronization.java
+++
@@ -1,9 +0,0 @@
-package org.drools.alternative.persistence;
-
-
-public interface TransactionSynchronization {    
-    
-    void beforeCompletion();
-    
-    void afterCompletion(int status);
-}
